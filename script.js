cb66faa4859117d0225b836a9572bf354c3cd369
Pradeep-Roxx <121031057+Pradeep-Roxx@users.noreply.github.com>
2023-01-18 13:10:17 +0530

    Product Slider With Star Rating E-Commerce
    Added Star Rating For The Product Out Of Five.

--- script.js
+++ script.js
@@ -26,6 +26,23 @@ function moveImage()
 
 }
 
+const stars=document.querySelectorAll('.star');
+const current_rating=document.querySelector('.current-rat');
+stars.forEach((star,index)=>{
+    star.addEventListener('click',()=>{
+        let current_star=index+1;
+        current_rating.innerText=`${current_star} of 5`;
+
+        stars.forEach((star,i)=>{
+            if(current_star>=i+1){
+                star.innerHTML=`&#9733`;
+            }else{
+                star.innerHTML=`&#9734`;
+            }
+        })
+    })
+})
+
 const minusBtn=document.querySelector('.minus');
 const plusBtn=document.querySelector('.plus');
 const qtyTxt=document.querySelector('#qty');
@@ -47,3 +64,4 @@ plusBtn.addEventListener('click',()=>{
     }
 
 });
+
